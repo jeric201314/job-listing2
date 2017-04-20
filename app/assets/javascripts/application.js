14ec52cfb0db80beb82581eeb837be2236b633f7
hjeric201314 <hjeric201314@gmail.com>
2017-04-20 22:47:58 +0800

    first-version

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,26 @@
 //= require bootstrap/dropdown
 //= require bootstrap/modal
 //= require_tree .
+
+
+$("#toggle").click(function(event) {
+  event.preventDefault();
+  if ($(this).hasClass("isDown")) {
+    $(".navbar-fixed-top").animate({
+      "margin-top": "-62px"
+    }, "fast");
+    $("#content").animate({
+      "margin-top": "5px"
+    }, "fast");
+    $(this).removeClass("isDown");
+  } else {
+    $(".navbar-fixed-top").animate({
+      "margin-top": "0px"
+    }, "fast");
+    $("#content").animate({
+      "margin-top": "80px"
+    }, "fast");
+    $(this).addClass("isDown");
+  }
+  return false;
+});
